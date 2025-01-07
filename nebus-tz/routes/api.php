f438b36138638ed9c80a47801be9ec203616f9c4
Daulet <lepessov.daulet@gmail.com>
2025-01-07 20:55:56 +0500

    update

--- nebus-tz/routes/api.php
+++ nebus-tz/routes/api.php
@@ -4,25 +4,18 @@ use App\Http\Controllers\OrganizationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware([ \App\Http\Middleware\ApiKeyMiddleware::class])->group(function () {
 
-Route::get('organizations/building/{buildingId}', [OrganizationController::class, 'getOrganizationsByBuilding'])
-    ->middleware(\App\Http\Middleware\ApiKeyMiddleware::class);
+    Route::get('organizations/building/{buildingId}', [OrganizationController::class, 'getOrganizationsByBuilding']);
 
-Route::get('organizations/activity/{activityId}', [OrganizationController::class, 'getOrganizationsByActivity'])
-    ->middleware(\App\Http\Middleware\ApiKeyMiddleware::class);
+    Route::get('organizations/activity/{activityId}', [OrganizationController::class, 'getOrganizationsByActivity']);
 
-Route::get('organizations/activity-name', [OrganizationController::class, 'getOrganizationsByActivityName'])
-    ->middleware(\App\Http\Middleware\ApiKeyMiddleware::class);
+    Route::get('organizations/activity-name', [OrganizationController::class, 'getOrganizationsByActivityName']);
 
-Route::get('organization/{id}', [OrganizationController::class, 'getOrganization'])
-    ->middleware(\App\Http\Middleware\ApiKeyMiddleware::class);
+    Route::get('organization/{id}', [OrganizationController::class, 'getOrganization']);
 
-Route::get('organizations/search/{name}', [OrganizationController::class, 'searchOrganizationsByName'])
-    ->middleware(\App\Http\Middleware\ApiKeyMiddleware::class);
+    Route::get('organizations/search/{name}', [OrganizationController::class, 'searchOrganizationsByName']);
 
-Route::get('organizations/near', [OrganizationController::class, 'getOrganizationsInRadius'])
-    ->middleware(\App\Http\Middleware\ApiKeyMiddleware::class);
+    Route::get('organizations/near', [OrganizationController::class, 'getOrganizationsInRadius']);
 
+});
